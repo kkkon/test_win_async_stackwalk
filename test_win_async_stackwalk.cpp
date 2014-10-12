01ca2eabded3036c96d26d8cb6ea94b2657847a5
kkkon <kiyofumi.kondoh+github@gmail.com>
2014-10-13 03:00:25 +0900

    add Sym init term. add CreateProcess

--- test_win_async_stackwalk.cpp
+++ test_win_async_stackwalk.cpp
@@ -13,6 +13,8 @@ public:
     bool    attachProcess( const DWORD dwProcessId );
     bool    detachProcess( void );
 
+    bool    initDebugHelp();
+    bool    termDebugHelp();
 public:
     kkRemoteAsyncStackwalk();
     virtual ~kkRemoteAsyncStackwalk();
@@ -64,6 +66,7 @@ kkRemoteAsyncStackwalk::attachProcess( const DWORD dwProcessId )
             return false;
         }
     }
+    this->initDebugHelp();
 
     return true;
 }
@@ -72,6 +75,8 @@ bool
 kkRemoteAsyncStackwalk::detachProcess( void )
 {
     bool result = true;
+
+    this->termDebugHelp();
 
     if ( NULL != m_hProcess )
     {
@@ -96,8 +101,39 @@ kkRemoteAsyncStackwalk::detachProcess( void )
     return result;
 }
 
+bool
+kkRemoteAsyncStackwalk::initDebugHelp(void)
+{
+    if ( NULL == m_hProcess )
+    {
+        return false;
+    }
 
+    const BOOL BRet = ::SymInitializeW( m_hProcess, NULL, TRUE );
+    if ( FALSE == BRet )
+    {
+        return false;
+    }
 
+    return true;
+}
+
+bool
+kkRemoteAsyncStackwalk::termDebugHelp(void)
+{
+    if ( NULL == m_hProcess )
+    {
+        return false;
+    }
+
+    const BOOL BRet = ::SymCleanup( m_hProcess );
+    if ( FALSE == BRet )
+    {
+        return false;
+    }
+
+    return true;
+}
 
 
 
@@ -123,8 +159,43 @@ int _tmain(int argc, _TCHAR* argv[])
 
     if ( 0 == dwProcessId )
     {
+        STARTUPINFOW    startupInfo;
+        ZeroMemory( &startupInfo, sizeof(startupInfo) );
+        startupInfo.cb = sizeof(startupInfo);
+        PROCESS_INFORMATION     processInfo;
+        ZeroMemory( &processInfo, sizeof(processInfo) );
+
+        wchar_t cmdLine[512];
+        cmdLine[0] = '\0';
+        {
+            LPWSTR strCmdLine = ::GetCommandLineW();
+            if ( NULL != strCmdLine )
+            {
+                const wchar_t* p = ::wcsstr( strCmdLine, argv[1] );
+                p += ::wcslen( argv[1] );
+                ::wcscpy_s( cmdLine, sizeof(cmdLine)/sizeof(cmdLine[0]), p );
+            }
+        }
+
+        const BOOL BRet = ::CreateProcessW( argv[1], cmdLine, NULL, NULL, FALSE, 0, NULL, NULL, &startupInfo, &processInfo );
+        if ( FALSE == BRet )
+        {
+            const DWORD dwRet = ::GetLastError();
+            ::OutputDebugStringW( L"CreateProcess failed.\n" );
+        }
+        else
+        {
+            ::CloseHandle( processInfo.hThread );
+            ::CloseHandle( processInfo.hProcess );
+            dwProcessId = processInfo.dwProcessId;
+        }
     }
-    else
+
+    if ( 0 == dwProcessId )
+    {
+        return false;
+    }
+
     {
         remote.attachProcess( dwProcessId );
     }
